60a5c508c122776b625743a9a594a159f25b9358
Yasmani Rosado <tas185@gmail.com>
2026-04-03 12:28:35 -0500

    add mysql and testing with cloud discovery

---
+++ booking-microservice/src/main/java/org/yasmani/io/bookingmicroservice/BookingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.bookingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingMicroserviceApplication.class, args);
+    }
+
+}
---
+++ booking-microservice/src/test/java/org/yasmani/io/bookingmicroservice/BookingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.bookingmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

18e207e5b0b141042066c567acbf6d3b3fbc9a4c
Yasmani Rosado <tas185@gmail.com>
2026-04-03 17:58:19 -0500

    add configuration with search-path

--- config-microservice/src/main/java/org/yasmani/io/configmicroservice/ConfigMicroserviceApplication.java
+++ config-microservice/src/main/java/org/yasmani/io/configmicroservice/ConfigMicroserviceApplication.java
@@ -2,8 +2,10 @@ package org.yasmani.io.configmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigMicroserviceApplication {
 
   public static void main(String[] args) {

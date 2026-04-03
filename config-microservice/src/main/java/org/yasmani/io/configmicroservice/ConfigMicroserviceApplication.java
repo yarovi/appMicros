948236b5a2817cb9c234c1d037513066c479ec60
Yasmani Rosado <tas185@gmail.com>
2026-04-03 17:46:46 -0500

    add configuration config

---
+++ config-microservice/src/main/java/org/yasmani/io/configmicroservice/ConfigMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.configmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigMicroserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigMicroserviceApplication.class, args);
+  }
+
+}
---
+++ config-microservice/src/test/java/org/yasmani/io/configmicroservice/ConfigMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.configmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigMicroserviceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

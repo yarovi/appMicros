6b1a7b62f12c7baee04d37b3622fd335160b3a64
Yasmani Rosado <tas185@gmail.com>
2026-04-03 12:06:50 -0500

    add discovery micro

---
+++ discovery-microservice/src/test/java/org/yasmani/io/discoverymicroservice/DiscoveryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.discoverymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

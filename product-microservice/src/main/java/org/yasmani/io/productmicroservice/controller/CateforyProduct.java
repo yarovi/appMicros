de3318bf6619610a74bf52988cdd1e8242e1f643
Yasmani Rosado <tas185@gmail.com>
2026-04-05 21:07:59 -0500

    Add pom parent

---
+++ product-microservice/src/main/java/org/yasmani/io/productmicroservice/controller/CateforyProduct.java
@@ -0,0 +1,10 @@
+package org.yasmani.io.productmicroservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/category" )
+public class CateforyProduct {
+}
